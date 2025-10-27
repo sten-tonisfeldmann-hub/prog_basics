9a2b7f1bf18b944958e822f5b3afcc56fd9423ea
Sten <sten-tonis.feldmann@VOCO.ee>
2025-10-27 12:42:09 +0200

    Ülesanne: for-tsükkel

---
+++ loop_exercises.py
@@ -0,0 +1,12 @@
+def cross_sum(numbers: str) -> int:
+    """
+    Return cross sum of numbers.
+
+    print(cross_sum("1234")) => 1 + 2 + 3 + 4 = 10
+    print(cross_sum("0")) => 0
+    print(cross_sum("4129458")) => 33
+    """
+    total = 0
+    
+    for num in range(numbers):
+        
